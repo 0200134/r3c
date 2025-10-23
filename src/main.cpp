f82e4a47a5ea505b7eb26527ea87dcff4ec2efbf
0200134 <84560987+0200134@users.noreply.github.com>
2025-10-23 18:32:16 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,6 @@
-// src/main.cpp
 #include <iostream>
 
 int main(int argc, char** argv) {
-    std::cout << "🧩 R3C LLVM-Free Universal Build" << std::endl;
-#if defined(_WIN32)
-    std::cout << "Platform: Windows" << std::endl;
-#elif defined(__APPLE__)
-    std::cout << "Platform: macOS" << std::endl;
-#elif defined(__linux__)
-    std::cout << "Platform: Linux" << std::endl;
-#else
-    std::cout << "Platform: Unknown" << std::endl;
-#endif
+    std::cout << "🧩 R3C Universal Entry Point" << std::endl;
     return 0;
 }
