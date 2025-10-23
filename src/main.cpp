f66df304850e9f2b28492e0d63586f76e0b46828
0200134 <84560987+0200134@users.noreply.github.com>
2025-10-23 18:38:22 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+// src/main.cpp
 #include <iostream>
 
 int main(int argc, char** argv) {
-    std::cout << "🧩 R3C Universal Entry Point" << std::endl;
+    std::cout << "🧩 R3C LLVM-Free Compiler Entry" << std::endl;
+#if defined(__APPLE__)
+    std::cout << "Running on macOS (AppleClang)" << std::endl;
+#elif defined(_WIN32)
+    std::cout << "Running on Windows (MSVC)" << std::endl;
+#elif defined(__linux__)
+    std::cout << "Running on Linux (GCC/Clang)" << std::endl;
+#else
+    std::cout << "Running on Unknown Platform" << std::endl;
+#endif
     return 0;
 }
