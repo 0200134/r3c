223b46b505b79df9c557bda39e686d58a0d5d5df
0200134 <84560987+0200134@users.noreply.github.com>
2025-10-23 18:27:32 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    std::cout << "🧩 R3C Compiler (LLVM-Free)" << std::endl;
+    std::cout << "🧩 R3C Compiler (LLVM-Free Build System)" << std::endl;
+    std::cout << "Platform: " 
+#ifdef _WIN32
+              << "Windows";
+#elif defined(__APPLE__)
+              << "macOS";
+#else
+              << "Linux";
+#endif
+    std::cout << std::endl;
     return 0;
 }
