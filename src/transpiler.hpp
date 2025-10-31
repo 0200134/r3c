c9a70b4bd6200b5e644cbb9c7fb8f96649d0b303
0200134 <84560987+0200134@users.noreply.github.com>
2025-11-01 06:06:34 +0900

    Update transpiler.hpp

--- src/transpiler.hpp
+++ src/transpiler.hpp
@@ -2,12 +2,14 @@
 #include <string>
 #include <filesystem>
 
+namespace fs = std::filesystem;
+
 namespace r3c {
 
 struct TranspileOptions {
     std::string input_path;
     std::string out_dir = "build/out";
-    bool emit_asm = true;
+    bool emit_asm  = true;
     bool emit_rust = true;
 };
 
@@ -16,9 +18,9 @@ public:
     int run(const TranspileOptions& opt);
 
 private:
-    bool emit_rust_stub(const std::filesystem::path& input, const std::filesystem::path& out_dir);
-    bool emit_nasm_stub(const std::filesystem::path& input, const std::filesystem::path& out_dir);
+    bool emit_rust_stub(const fs::path& input, const fs::path& out_dir);
+    bool emit_nasm_stub(const fs::path& input, const fs::path& out_dir);
+    std::string detect_rust_version();
 };
 
 } // namespace r3c
-
